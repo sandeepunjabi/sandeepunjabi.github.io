ba0f98814c3c57b8f7b316bf572f0dfd6aa033a4
sandeepunjabi <36820672+sandeepunjabi@users.noreply.github.com>
2019-12-30 06:45:09 +0530

    init firebase

--- welcome-baby/scripts.js
+++ welcome-baby/scripts.js
@@ -1,3 +1,17 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC4tooIIlF2KujkF5ABd5wFCjhkPDT98eE",
+  authDomain: "welcome-baby.firebaseapp.com",
+  databaseURL: "https://welcome-baby.firebaseio.com",
+  projectId: "welcome-baby",
+  storageBucket: "welcome-baby.appspot.com",
+  messagingSenderId: "637910658035",
+  appId: "1:637910658035:web:ea04167a06141972778086",
+  measurementId: "G-LE51SQX4ME"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 function toggleMessenger() {
   const messenger = document.querySelector('#messenger');
   const action = messenger.classList.contains('open') ? 'remove' : 'add';
