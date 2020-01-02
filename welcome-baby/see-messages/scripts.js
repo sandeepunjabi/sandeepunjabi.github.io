8552e50eb8985bfa5948b97e75ce43078de4a746
Sandeep Punjabi <sandeepunjabi@live.com>
2020-01-02 11:33:07 +0530

    adds a feature to display the saved messages

---
+++ welcome-baby/see-messages/scripts.js
@@ -0,0 +1,29 @@
+function renderMessages(messages) {
+  const html = [];
+  messages.forEach(({ body }) => {
+    html.push(`
+      <article class="message">
+        <h3>${body.name}</h3>
+        <p>${body.message}</p>
+      </article>
+    `);
+  });
+
+  document.querySelector('#messages').innerHTML = html.join('');
+}
+
+async function handleFormSubmit(e) {
+  e.preventDefault();
+
+  const token = e.target.querySelector('input').val;
+  const response = fetch(
+    'https://us-central1-welcome-baby.cloudfunctions.net/getMessages',
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
+  const data = response.json();
+  renderMessages(Object.values(data));
+}
+
+document
+  .querySelector('#intro form')
+  .addEventListener('submit', handleFormSubmit);
